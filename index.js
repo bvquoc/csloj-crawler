58844cd0cb1a37d45d3fd195894d05ad89a49e4e
bvquoc <quocpro10.02.2003@gmail.com>
2021-08-21 23:52:50 +0700

    add main code (index.js)

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
-const cheerio = require('cheerio');
-const fs = require('fs');
-const request = require('request-promise');
+const cheerio = require("cheerio");
+const fs = require("fs");
+const request = require("request-promise");
 
 // const endText = `\n\n**Nguồn:** [Chuyên Sơn La Online Judge](http://csloj.ddns.net/)
 // \n**Group giải đáp thắc mắc:** [Lập trình 24h](https://www.facebook.com/groups/1386904321519984)`;
 
-const parDir = './data';
+const parDir = "./data";
 const problemIDList = [];
 const MAX_ID = 10018;
 
 for (let id = 1; id <= MAX_ID; id++) {
-  request(`http://csloj.ddns.net/problem/${id}/edit`, (error, response, html) => {
-    if (!error && response.statusCode == 200) {
-      const $ = cheerio.load(html); // load HTML
+  request(
+    `http://csloj.ddns.net/problem/${id}/edit`,
+    (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html); // load HTML
 
-      let content = $('.markdown-edit').text();
-      if (content !== '') {
-        problemIDList.push(id);
-        content = content.split('$').join('~');
-        content = content.split('\n#### ').join('\n## ');
-        content = content.split('\n##### ').join('\n#### ');
-        content = `${content}${endText}`;
-        console.log('Problem id', id, ' was crawled!');
-        const dir = `${parDir}/${id}`;
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir, { recursive: true });
+        let content = $(".markdown-edit").text();
+        if (content !== "") {
+          problemIDList.push(id);
+          content = content.split("$").join("~");
+          content = content.split("\n#### ").join("\n## ");
+          content = content.split("\n##### ").join("\n#### ");
+          // content = `${content}${endText}`;
+          console.log("Problem id", id, " was crawled!");
+          const dir = `${parDir}/${id}`;
+          if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+          }
+          fs.writeFileSync(`${dir}/desc.md`, content);
+        } else {
+          console.log("Problem id", id, " doesn't exist!");
         }
-        fs.writeFileSync(`${dir}/desc.md`, content);
       } else {
-        console.log('Problem id', id, " doesn't exist!");
+        console.log("Can't cralw", id);
+        // console.log(error);
       }
-    } else {
-      console.log("Can't cralw", id);
-      // console.log(error);
     }
-  });
+  );
 }
 
 // fs.writeFileSync(`${parDir}/problemIDList.json`, JSON.stringify(problemIDList));
