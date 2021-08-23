fd841f03417969bd7a46aed04a9abfe17ec30765
bvquoc <quocpro10.02.2003@gmail.com>
2021-08-23 13:54:16 +0700

    add get-description module

--- get-description.js
+++ get-description.js
@@ -2,38 +2,25 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const request = require('request-promise');
 
-// const src = `\n\n**Nguồn:** [Chuyên Sơn La Online Judge](http://csloj.ddns.net/)`
-
-const parDir = './data';
-const problemIDList = [];
-const MAX_ID = 50; // 10018;
-
-for (let id = 1; id <= MAX_ID; id++) {
+function getDescription(dir, id) {
   request(`http://csloj.ddns.net/problem/${id}/edit`, (error, response, html) => {
     if (!error && response.statusCode == 200) {
       const $ = cheerio.load(html); // load HTML
 
       let content = $('.markdown-edit').text();
       if (content !== '') {
-        problemIDList.push(id);
-        // content = content.split("$").join("~");
         content = content.split('\n#### ').join('\n## ');
-        content = content.split('\n##### ').join('\n#### ');
-        // content = `${content}${src}`;
-        console.log('Problem id', id, ' was crawled!');
-        const dir = `${parDir}/${id}`;
-        if (!fs.existsSync(dir)) {
-          fs.mkdirSync(dir, { recursive: true });
-        }
+        content = content.split('\n##### ').join('\n### ');
+
+        console.log(`Problem ${id}: was crawled description!`);
         fs.writeFileSync(`${dir}/desc.md`, content);
       } else {
         console.log('Problem id', id, " doesn't exist!");
       }
     } else {
-      console.log("Can't cralw", id);
-      // console.log(error);
+      console.log(`Problem ${id}: can't crawl!`);
     }
   });
 }
 
-fs.writeFileSync(`${parDir}/problemIDList.json`, JSON.stringify(problemIDList));
+module.exports = getDescription;
