0b3191ae32e35a752770f6d9ecf547350883fc88
bvquoc <quocpro10.02.2003@gmail.com>
2021-08-23 13:54:54 +0700

    add main code (index.js)

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const getDescription = require('./get-description');
+const getInfo = require('./get-info');
+
+let IDList = [];
+function loadData() {
+  var dataContent = fs.readFileSync('./JSON/IDList.json');
+  IDList = JSON.parse(dataContent);
+}
+
+loadData();
+
+const parDir = './data';
+
+for (const id of IDList) {
+  const dir = `${parDir}/${id}`;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir, { recursive: true });
+    getDescription(dir, id);
+    getInfo(dir, id);
+  }
+}
